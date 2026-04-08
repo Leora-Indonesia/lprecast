add28b7f9c467ec503e1d0d1c7188e372d34655d
Alvin Al <alvvxal@gmail.com>
2026-04-08 15:16:32 +0700

    feat: add PWA offline support

---
+++ app/~offline/layout.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Offline | LPrecast",
+  description: "Anda sedang offline, periksa koneksi internet",
+}
+
+export default function OfflineLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return children
+}
